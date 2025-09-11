939918552a8b74c631a4bdacadc9d9c5aef599d6
Guilherme Almeida <guilhermecamargo_@outlook.com>
2025-09-11 15:45:15 -0300

    fix[CK-16, 11]: Correção do Design Padrão do projeto, implementação da lógica para renderização condicional das páginas que exibem tarefas;
    Co-Authored-By: Gustavo Almeida Camargo <88459386+GustavoAC0802@users.noreply.github.com>
    Co-Authored-By: Valério <200152668+mariannevalerion@users.noreply.github.com>

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,20 +1,59 @@
 import Navbar from "../components/Navbar/Navbar";
 import profileUser from '../assets/profile-user.png'
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
+import { useState } from "react";
+import Modal from "../components/Modal/Modal";
+import TaskList from "../components/TaskList/TaskList";
+
 
 export default function RootLayout() {
+  const pageLink = useLocation();
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const buttonFunction = () => {
+    setIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="flex flex-col ml-[20%] w-[80%] h-dvh gap-2">
         <div className="flex border-b-[1px] border-gray-200 p-2 items-center justify-end h-16">
-            <div className="flex gap-4 text-black items-center font-bold">
-              <img src={profileUser} alt="" className="h-6"/>
-              <p>Otávio Vianna Lima</p>
-            </div>
+          <div className="flex gap-4 text-black items-center font-bold">
+            <img src={profileUser} alt="" className="h-6" />
+            <p>Otávio Vianna Lima</p>
+          </div>
         </div>
-        <Outlet/>
+        <Header btnFunc={buttonFunction} />
+        {pageLink.pathname === '/' ? (
+          <div className="w-full justify-evenly pb-3 flex">
+            <TaskList
+              title="Não Iniciada"
+              taksCount={2}
+            />
+            <TaskList
+              title="Em Andamento"
+              taksCount={10}
+            />
+            <TaskList
+              title="Concluída"
+              taksCount={1}
+            />
+          </div>
+        ):(
+          <Outlet/>
+        )}
+        <Modal
+          tipoModal="Nova"
+          condicaoModal={isOpen}
+          closeModal={closeModal}
+        />
       </div>
     </>
   )
