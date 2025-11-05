5caf0b5577fe63c5ff9a656ccc1d036546e73b88
pedro-h-martins <pedrohenrimartinss@gmail.com>
2025-11-05 15:55:57 -0300

    chore: Restaurar mensagem de tarefas

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export default function Header({ btnFunc }: HeaderProps) {
   const location = useLocation();
 
-  const headerTitle: Record<string, string> ={
+  const headerTitle: Record<string, string> = {
     '/home/todas-tarefas': 'Todas as tarefas',
     '/home': 'Minhas tarefas',
     '/home/log-auditoria': 'Log de auditoria'
@@ -22,10 +22,13 @@ export default function Header({ btnFunc }: HeaderProps) {
     <>
       <div className="w-full h-auto flex items-center justify-center">
         <div className="w-full flex items-center justify-between p-2 max-[800px]:flex-col max-[800px]:gap-2">
-          <div className="flex flex-col items-start jutify-center max-[800px]:w-full max-[800px]:justify-start">
-            <h1 className="text-gray-800 text-4xl font-bold max-[800px]:text-2xl">{titulo}</h1>
-          </div>
-          
+          {buttonAppearsAt.includes(location.pathname) && (
+            <div className="flex flex-col items-start justify-center max-[800px]:w-full max-[800px]:justify-start">
+              <h1 className="text-gray-800 text-4xl font-bold max-[800px]:text-2xl">{titulo}</h1>
+              <p className="text-gray-500 text-lg pl-1 max-[800px]:text-sm">Gerencie e acompanhe o progresso de suas atividades</p>
+            </div>
+          )}
+
           <div className="flex gap-2 items-center max-[800px]:w-full max-[800px]:justify-end">
             {showNotifications && <NotificationBell />}
             {buttonAppearsAt.includes(location.pathname) && (
